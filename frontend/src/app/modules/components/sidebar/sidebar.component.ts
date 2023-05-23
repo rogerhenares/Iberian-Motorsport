3db50aac44bbc34c91bca83e2bc01d64e5eb04f3
Roger Henares <rogerhcab@gmail.com>
2023-05-23 13:46:37 +0200

    Tocanto cosas

--- frontend/src/app/modules/components/sidebar/sidebar.component.ts
+++ frontend/src/app/modules/components/sidebar/sidebar.component.ts
@@ -8,10 +8,10 @@ declare interface RouteInfo {
     class: string;
 }
 export const ROUTES: RouteInfo[] = [
-    { path: '/dashboard', title: 'Dashboard',  icon: 'dashboard', class: '' },
-    { path: '/user-profile', title: 'User Profile',  icon:'person', class: '' },
-    { path: '/table-list', title: 'Table List',  icon:'content_paste', class: '' },
-    { path: '/typography', title: 'Typography',  icon:'library_books', class: '' },
+    { path: '/dashboard', title: 'Campeonatos',  icon: 'dashboard', class: '' },
+    { path: '/user-profile', title: 'Perfil',  icon:'person', class: '' },
+    { path: '/table-list', title: 'Carreras',  icon:'content_paste', class: '' },
+    { path: '/typography', title: 'Guias',  icon:'library_books', class: '' },
     { path: '/icons', title: 'Icons',  icon:'bubble_chart', class: '' },
     { path: '/maps', title: 'Maps',  icon:'location_on', class: '' },
     { path: '/notifications', title: 'Notifications',  icon:'notifications', class: '' },
