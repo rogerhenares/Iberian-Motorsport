5140d65a0979e3dd16b9624b4b3098255b5570d8
Javier Sardiñas <javiersardinashurtado@gmail.com>
2023-12-22 17:48:17 +0100

    negruzco version 1

--- frontend/src/app/modules/summary/summary.component.ts
+++ frontend/src/app/modules/summary/summary.component.ts
@@ -1,6 +1,10 @@
-import {Component, OnInit} from '@angular/core';
+import {Component, OnInit, ViewChild, ElementRef, AfterViewInit} from '@angular/core';
 import {Router} from '@angular/router';
 import {AppContext} from "../../util/AppContext";
+import Swiper from 'swiper'
+import { register } from 'swiper/element/bundle';
+
+register();
 
 
 @Component({
@@ -9,10 +13,24 @@ import {AppContext} from "../../util/AppContext";
     styleUrls: ['./summary.component.css']
 })
 export class SummaryComponent implements OnInit {
+    @ViewChild('imagenEspecial') imagenEspecial: ElementRef;
+
     constructor(
         public router: Router,
-        public appContext : AppContext
+        public appContext : AppContext,
     ) {}
+
+    ngAfterViewInit(): void {
+        new Swiper('.swiper-container', {
+            slidesPerView: 4, // Mostrar 4 cartas a la vez
+            spaceBetween: 50, // Espacio entre las cartas
+            navigation: {
+              nextEl: '.swiper-button-next', // Botón para ir a la siguiente carta
+              prevEl: '.swiper-button-prev', // Botón para ir a la carta anterior
+            },
+          });
+    }
+    
 
     ngOnInit(): void {
     }
