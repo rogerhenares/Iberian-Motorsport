8488a78a169bd4fcf98c22468dc7e7128729c16c
Guillermo Díaz <gdiazbricio@gmail.com>
2024-01-15 19:32:21 +0000

    javi es MUY puto

--- frontend/src/app/modules/summary/summary.component.ts
+++ frontend/src/app/modules/summary/summary.component.ts
@@ -1,38 +1,63 @@
-import {Component, OnInit, ViewChild, ElementRef, AfterViewInit} from '@angular/core';
-import {Router} from '@angular/router';
-import {AppContext} from "../../util/AppContext";
-import Swiper from 'swiper'
-import { register } from 'swiper/element/bundle';
-
-register();
-
+import { Component, OnInit, ViewChild, ElementRef, AfterViewInit } from '@angular/core';
+import { Router } from '@angular/router';
+import { AppContext } from "../../util/AppContext";
+import { trigger, transition, style, animate } from '@angular/animations';
 
 @Component({
-    selector: 'app-summary',
-    templateUrl: './summary.component.html',
-    styleUrls: ['./summary.component.css']
+  selector: 'app-summary',
+  templateUrl: './summary.component.html',
+  styleUrls: ['./summary.component.css'],
+  animations: [
+    trigger(
+      'inOutAnimation',
+      [
+        transition(
+          ':enter',
+          [
+            style({ opacity: 0 }),
+            animate('0.3s ease-out',
+              style({ opacity: 1 }))
+          ]
+        ),
+      ]
+    )
+  ]
 })
-export class SummaryComponent implements OnInit {
-    @ViewChild('imagenEspecial') imagenEspecial: ElementRef;
+export class SummaryComponent implements OnInit, AfterViewInit {
+  @ViewChild('imagenEspecial') imagenEspecial: ElementRef;
 
-    constructor(
-        public router: Router,
-        public appContext : AppContext,
-    ) {}
+  constructor(
+    public router: Router,
+    public appContext: AppContext,
+  ) {}
 
-    ngAfterViewInit(): void {
-        new Swiper('.swiper-container', {
-            slidesPerView: 4, // Mostrar 4 cartas a la vez
-            spaceBetween: 50, // Espacio entre las cartas
-            navigation: {
-              nextEl: '.swiper-button-next', // Botón para ir a la siguiente carta
-              prevEl: '.swiper-button-prev', // Botón para ir a la carta anterior
-            },
-          });
-    }
-    
 
-    ngOnInit(): void {
-    }
+  information = [
+    { type: 'screen1' },
+    { type: 'screen2' },
+    { type: 'screen3' }
+  ];
+
+  currentIndex = 0;
+
+  showNext() {
+    this.currentIndex = (this.currentIndex + 1) % this.information.length;
+  }
+
+  showPrev() {
+    this.currentIndex = (this.currentIndex - 1 + this.information.length) % this.information.length;
+  }
+
+
+  ngAfterViewInit(): void {
+    // Aquí puedes poner el código relacionado con el DOM después de la vista.
+  }
+
+  ngOnInit(): void {
+    // Aquí puedes poner el código de inicialización.
+  }
+
 }
+
+
 
