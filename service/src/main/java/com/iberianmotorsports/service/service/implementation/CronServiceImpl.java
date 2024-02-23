45279bbc1f294a4f091543a19e37f75faeab5d4a
abel <abeljdrs@hotmail.com>
2024-02-23 21:16:23 +0100

    [back] fix stop server logic after race finished

--- service/src/main/java/com/iberianmotorsports/service/service/implementation/CronServiceImpl.java
+++ service/src/main/java/com/iberianmotorsports/service/service/implementation/CronServiceImpl.java
@@ -8,15 +8,15 @@ import com.iberianmotorsports.service.utils.RaceStatus;
 import lombok.AllArgsConstructor;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
-import org.springframework.scheduling.annotation.Async;
 import org.springframework.scheduling.annotation.Scheduled;
 import org.springframework.stereotype.Service;
 
+import java.io.BufferedReader;
 import java.io.File;
+import java.io.IOException;
+import java.io.InputStreamReader;
 import java.time.LocalDateTime;
 import java.util.List;
-import java.util.concurrent.ExecutorService;
-import java.util.concurrent.Executors;
 
 @Service
 @AllArgsConstructor
@@ -57,7 +57,46 @@ public class CronServiceImpl implements CronService {
     }
 
     @Override
-    @Scheduled(cron = "0 0/5 * * * *", zone = "Europe/Madrid")
+    @Scheduled(cron = "0 0/1 * * * *", zone = "Europe/Madrid")
+    public void stopServerACC() {
+        if(serverProperty.getCronEnabled()){
+            List<Race> launchedRaceList = raceService.getRaceByStatusAndDate(RaceStatus.LAUNCHED, LocalDateTime.now());
+            for (Race raceToStop: launchedRaceList) {
+                if(importDataService.isResultsReady(raceToStop)) {
+                    logger.info("CRON JOB: STOP RACE ID -> {} TRACK -> {}", raceToStop.getId(), raceToStop.getTrack());
+                    ProcessBuilder getPidProcessBuilder = new ProcessBuilder("cmd.exe", "/c", "netstat -aon | findstr :9262");
+
+                    try {
+                        Process portProcess = getPidProcessBuilder.start();
+                        BufferedReader reader = new BufferedReader(new InputStreamReader(portProcess.getInputStream()));
+                        String line;
+                        String serverPID="";
+                        while ((line = reader.readLine()) != null) {
+                            if (line.contains(":9262")) {
+                                String[] parts = line.trim().split("\\s+");
+                                if (parts.length >= 5) {
+                                    serverPID = parts[parts.length - 1];
+                                }
+                            }
+                        }
+                        if(serverPID.isBlank()) {
+                            logger.info("CRON JOB: STOP PROCESS FAILED RACE ID -> {}", raceToStop.getId());
+                        }else{
+                            ProcessBuilder stopServerBuilder = new ProcessBuilder("cmd.exe", "/c", "taskkill /f /pid " + serverPID);
+                            stopServerBuilder.start();
+                            raceService.setRaceStatus(raceToStop, RaceStatus.STOP);
+                        }
+                    } catch (IOException e) {
+                        logger.info("CRON JOB: STOP FAILED RACE ID -> {} TRACK -> {} ERROR -> {}", raceToStop.getId(), raceToStop.getTrack(), e.getMessage());
+                        raceService.setRaceStatus(raceToStop, RaceStatus.STOP_FAILED);
+                    }
+                }
+            }
+        }
+    }
+
+    @Override
+    @Scheduled(cron = "0 0/1 * * * *", zone = "Europe/Madrid")
     public void importServerResultsACC() {
         if(serverProperty.getCronEnabled()){
             List<Race> importRaceList = raceService.getRaceByStatusAndDate(RaceStatus.STOP, LocalDateTime.now());
